415fe5fcb0b8ca9a06b93c05480e2e49529615ea
Alexander Watson MCS2019 <a.watson.2019@uni.strath.ac.uk>
2021-03-04 15:27:14 +0000

    Add new file

---
+++ Players.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Players {
+
+	ArrayList<Player>[] allPlayers;  // not sure if this is how we store players
+	private String playerFile;
+	
+	
+	private Players() {
+		
+	}
+	
+	private void addPlayer(Player p) {
+		
+	}
+
+	private void savePlayers() {
+		
+	}
+	
+	private void findPlayer(Player p) {
+		
+	}
+	
+	private void getAllPlayersAccuracies() {
+		
+	}
+	
+	private void getAllPlayersCryptogramsPlayed() {
+		
+	}
+
+	private void getAllPlayersCryptogramsCompleted() {
+	
+	}
+}
