e9f8f735716bfbf527b9ff1a37e272b5ea88985b
Scott Love CSM2019 <scott.love.2019@uni.strath.ac.uk>
2021-03-05 11:05:07 +0000

    Update Cryptogram.java

--- Cryptogram.java
+++ Cryptogram.java
@@ -7,7 +7,6 @@ import java.util.Scanner;
 public class Cryptogram {
 
 	private String phrase;
-	private String cryptogramAlphabet;
 	public int frequency;
 	
 	public Cryptogram() {
@@ -15,6 +14,7 @@ public class Cryptogram {
 		this.encrypt(phrase);
 	}
 	
+	// reads a phrase (currently only one) from a text file
 	public void getPhrase() {
 		try {
 			File myObj = new File("C:\\Users\\scott\\Desktop\\hello.txt");
@@ -31,20 +31,53 @@ public class Cryptogram {
 	}
 	
 	public void encrypt(String phrase) {
-		char[] encryptPhrase = phrase.toUpperCase().toCharArray();
+		
+		// usedChars is used as referral to ensure duplicate characters have the same mapping
+		char[] usedChars = phrase.toUpperCase().toCharArray();
+		// encChars stores the encrypted phrase 
+		char[] encChars = new char[usedChars.length];
+		// numsUsed stores the random numbers used so mapping cannot be repeated 
+		ArrayList<Integer> numsUsed = new ArrayList<Integer>();
 		
 		for (int i = 0; i < phrase.length(); i++) {
-			int a = (int) encryptPhrase[i];
-
+			
+			// Generates random numbers
 			Random r = new Random();
 			int low = 65;
 			int high = 91;
-			int result = r.nextInt(high-low) + low;
+			int result = r.nextInt(high - low) + low;
 			
-			char b = (char) result;
-			
-			System.out.println(encryptPhrase[i] + " : " + a + " : " + result + " : " + b);
+			// If already used in a mapping, another random number is chosen 
+			if (numsUsed.contains(result)) {
+				while (numsUsed.contains(result)) {
+					result = r.nextInt(high-low) + low;
+				}
+			}
+			// Converts the random number generated into a character 
+			char newChar = (char) result;
+			// Base case - first entry has nothing to compare to so it can always be set
+			if (i == 0) {
+				encChars[i] = newChar;
+				numsUsed.add(((int) newChar));
+			}
+			// Compares an element with all of the previous elements 
+			for (int j = 0; j < i; j++) { 
+				// If it appeared previously, the mapping is copied 
+				if (usedChars[i] == usedChars [j]) {
+					encChars[i] = encChars[j];
+					break;
+				}
+				// Otherwise a new mapping is made
+				else {
+					encChars[i] = newChar;
+					numsUsed.add(((int) newChar));
+				}
+			}	
 		}
+		
+		// TESTING ONLY
+		System.out.println(usedChars);
+		System.out.println(encChars);
 	}
 	
 	public int getFrequency() {
