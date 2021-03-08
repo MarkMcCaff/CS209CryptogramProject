7c52a72a25b9da8c0b3a4eb11063bd4408d2fc5e
Scott Love CSM2019 <scott.love.2019@uni.strath.ac.uk>
2021-03-08 00:50:45 +0000

    Add new file

---
+++ Player.java
@@ -0,0 +1,49 @@
+public class Player {
+	
+	private String username;
+	private int accuracy;
+	private int totalGuesses;
+	private int correctGuesses;
+	private int cryptogramsPlayed;
+	private int cryptogramsCompleted;
+	
+	public Player(String name) {
+		username = name;
+		totalGuesses = 0;
+		cryptogramsPlayed = 0;
+		cryptogramsCompleted = 0;
+	}
+	
+	public void incremementCryptogramsCompleted() {
+		cryptogramsCompleted++;
+	}
+	
+	public void incremementCryptogramsPlayed() {
+		cryptogramsPlayed++;
+	}
+	
+	public void incrementGuesses() {
+		totalGuesses++;
+	}
+	
+	public void incrementCorrGuesses() {
+		totalGuesses++;
+		correctGuesses++;
+	}
+	
+	public int getAccuracy() {
+		return ((correctGuesses / totalGuesses) * 100);
+	}
+	
+	public int getNumCryptogramsCompleted() {
+		return cryptogramsCompleted;
+	}
+	
+	public int getNumCryptogramsPlayed() {
+		return cryptogramsPlayed;
+	}
+	
+	public int getGuesses() {
+		return totalGuesses;
+	}
+}
