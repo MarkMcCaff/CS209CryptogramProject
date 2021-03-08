3828e4c1d413b38c20e3f6b77d274d8df24f2216
Scott Love CSM2019 <scott.love.2019@uni.strath.ac.uk>
2021-03-08 00:46:45 +0000

    Add new file

---
+++ Cryptogram.java
@@ -0,0 +1,63 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Cryptogram {
+
+	protected String phrase;
+	public int frequency;
+	public char[] encryptedPhrase;
+	public int[] intEncryptedPhrase;
+	public ArrayList<String> phrases;
+	
+	public Cryptogram() {
+		this.createPhrase();
+	}
+	
+	// reads a phrase (currently only one) from a text file
+	public void createPhrase() {
+		try {
+			File myObj = new File("C:\\Users\\scott\\Desktop\\hello.txt");
+			phrases = new ArrayList<String>();
+			Scanner sc = new Scanner(myObj);
+				while (sc.hasNextLine()) {
+					String data = sc.nextLine();
+					phrases.add(data);
+				}
+			sc.close();
+			if (phrases.isEmpty()) {
+				System.out.println("No phrases detected - Now exiting...");
+				System.exit(0);
+			}
+			// Creates a random number to choose the phrase 
+			Random r = new Random();
+			int low = 0;
+			int high = phrases.size();
+			int result = r.nextInt(high - low) + low;
+			phrase = phrases.get(result);
+		}
+		catch (FileNotFoundException e) {
+			System.out.println("An error occurred.");
+		}
+	}
+	
+	public int getFrequency() {
+		return frequency;
+	}
+	
+	public String getPhrase() {
+		return phrase;
+	}
+	
+	// If the puzzle is alphabetical, it will call this method 
+	public char[] getEncryption() {
+		return encryptedPhrase;
+	}
+
+	// If the puzzle is numerical, it will call this method 
+	public int[] getIntEncryption() {
+		return intEncryptedPhrase;
+	}
+}
