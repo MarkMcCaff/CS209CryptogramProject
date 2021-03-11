485b2152f3c3f4408ac63be1a44cb002abcf8e35
Alexander Watson MCS2019 <a.watson.2019@uni.strath.ac.uk>
2021-03-11 16:32:34 +0000

    Update to savePlayers method

--- Players.java
+++ Players.java
@@ -7,24 +7,44 @@ public class Players implements Player{
 
 	public ArrayList <String> playerList;	
 	
+	
+	
 	public void addPlayer(Player p, Scanner sc){
 		System.out.prinln("Enter your desired username: ");
 		String newUserName = sc.next();
 		try {
      	FileWriter myWriter = new FileWriter("C:\\Users\\alex\\Desktop\\players.txt");
-     	 	myWriter.write(newUserName + cryptogramsPlayed + cryptogramsCompleted + correctGuesses);
+     	 	myWriter.write(newUserName);
      		myWriter.close();
      		System.out.println("Successfully created new player: " + newUserName);
+     		username = newUserName;
     	catch (IOException e) {
      		System.out.println("An error occurred.");
 		    e.printStackTrace();
 		    }
-	
-	
 	}
-	
-	
-	
+		
+		
+	public void savePlayers() {
+		if (Game.savegame()) {
+			File myObj = new File("C:\\Users\\alex\\Desktop\\players.txt");
+		for(int i = 0; i < playerList.length; i++) {
+			if(playerList[i] = username) {
+				// overwrite the stored data for the user with new data
+			}
+			else {
+				myWriter.write(username + cryptogramsCompleted + cryptogramsPlayed + correctGuesses );
+	     		myWriter.close();
+			}
+				
+			}
+		}
+		
+		}
+			myWriter.write(newUserName + cryptogramsPlayed + cryptogramsCompleted + correctGuesses);
+		}
+		
+	}
 
 	public void findPlayer(Player p) {
 		try {
