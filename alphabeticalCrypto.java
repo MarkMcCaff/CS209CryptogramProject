0e8e427bbcd6c2c007b1fbae005e4829ccdec1a0
Scott Love CSM2019 <scott.love.2019@uni.strath.ac.uk>
2021-03-19 02:04:23 +0000

    Update alphabeticalCrypto.java

--- alphabeticalCrypto.java
+++ alphabeticalCrypto.java
@@ -4,63 +4,81 @@ import java.util.Random;
 public class alphabeticalCrypto extends Cryptogram {
 
 	public alphabeticalCrypto() {
-		this.getPhrase();
 		this.encrypt(phrase);
 	}
 	
+	// usedChars is used as referral to ensure duplicate characters have the same mapping
+	char[] usedChars = getPhrase().toCharArray();
+	// encChars stores the encrypted phrase 
+	char[] encChars = new char[usedChars.length];
+	// numsUsed stores the random numbers used so mapping cannot be repeated 
+	ArrayList<Integer> numsUsed = new ArrayList<Integer>();
+	
 	public void encrypt(String phrase) {
-		// usedChars is used as referral to ensure duplicate characters have the same mapping
-		char[] usedChars = phrase.toUpperCase().toCharArray();
-		// encChars stores the encrypted phrase 
-		char[] encChars = new char[usedChars.length];
-		// numsUsed stores the random numbers used so mapping cannot be repeated 
-		ArrayList<Integer> numsUsed = new ArrayList<Integer>();
 		for (int i = 0; i < phrase.length(); i++) {
 			// If the phrase has an empty space, the puzzle must also show this 
 			if (usedChars[i] == ' ') {
 				encChars[i] = ' ';
 			}
 			else {
-				// Generates random numbers
-				Random r = new Random();
-				int low = 65;
-				int high = 91;
-				int result = r.nextInt(high - low) + low;
-				// If already used in a mapping, another random number is chosen 
-				if (numsUsed.contains(result)) {
-					while (numsUsed.contains(result)) {
-						result = r.nextInt(high-low) + low;
-					}
-				}
-				// Converts the random number generated into a character 
-				char newChar = (char) result;
-				// Base case - first entry has nothing to compare to so it can always be set
-				if (i == 0) {
-					encChars[i] = newChar;
-					numsUsed.add(((int) newChar));
-				}
-				// Compares an element with all of the previous elements 
-				for (int j = 0; j < i; j++) { 
-					// If it appeared previously, the mapping is copied 
-					if (usedChars[i] == usedChars [j]) {
-						encChars[i] = encChars[j];
-						break;
-					}
-					// Otherwise a new mapping is made
-					else {
-						encChars[i] = newChar;
-						numsUsed.add(((int) newChar));
-					}
-				}	
+				compareElements(i);
 			}
 		}
-		// Prints the results of the mapping 
-		System.out.println("Encoded phrase: ");
+		printMapping();
+	}
+	
+	public void compareElements(int position) {
+		char newChar = generateLetter();
+		// Base case - first entry has nothing to compare to so it can always be set
+		if (position == 0) {
+			encChars[position] = newChar;
+			numsUsed.add(((int) newChar));
+		}
+		// Compares an element with all of the previous elements 
+		for (int j = 0; j < position; j++) { 
+			// If it appeared previously, the mapping is copied 
+			if (usedChars[position] == usedChars [j]) {
+				encChars[position] = encChars[j];
+				break;
+			}
+			// Otherwise a new mapping is made
+			else {
+				encChars[position] = newChar;
+				numsUsed.add(((int) newChar));
+			}
+		}
+	}
+	
+	// method for generating a letter which hasn't been used yet
+	public char generateLetter() {
+		int result = randomNum();
+		// If already used in a mapping, another random number is chosen 
+		if (numsUsed.contains(result)) {
+			while (numsUsed.contains(result)) {
+				result = randomNum();
+			}
+		}
+		// Converts the random number generated into a character 
+		return (char) result;
+	}
+	
+	// method for generating a random number
+	public int randomNum() {
+		Random r = new Random();
+		// low and high numbers are set as according to the alphabet (to ensure the mapping is uppercase)
+		int low = 65;
+		int high = 91;
+		return r.nextInt(high - low) + low;
+	}
+	
+	// Method for printing the results of the mapping
+	private void printMapping() {
+		System.out.print("Encoded phrase: ");
 		for (int i = 0; i < encChars.length; i++) {
 			System.out.print(encChars[i]);
 		}
 		// Clones the result of the mapping to a variable from the overall Cryptogram class
 		encryptedPhrase = encChars.clone();
 		System.out.println(" ");
-	}
+	}	
 }
