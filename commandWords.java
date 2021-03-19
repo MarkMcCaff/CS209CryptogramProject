f7e76632b298fb7f856c2861041566e3b5a1731e
Scott Love CSM2019 <scott.love.2019@uni.strath.ac.uk>
2021-03-19 02:06:31 +0000

    Add new file

---
+++ commandWords.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+
+public class commandWords {
+	// A mapping between a command word and the commandWord associated with it 
+	private HashMap<String, commandWord> validCommands;
+	
+	public commandWords() {
+        validCommands = new HashMap<>();
+        validCommands.put("guess", commandWord.GUESS);
+        validCommands.put("save", commandWord.SAVE);
+        validCommands.put("undo", commandWord.UNDO);
+        validCommands.put("load", commandWord.LOAD);
+        validCommands.put("exit", commandWord.EXIT);
+        validCommands.put("help", commandWord.HELP);
+    }
+
+	public commandWord getCommandWord(String commandWord) {
+        commandWord command = validCommands.get(commandWord);
+        if(command != null) {
+            return command;
+        }
+        else {
+            return command.UNKNOWN;
+        }
+    }
+    
+    public boolean isCommand(String aString) {
+        return validCommands.containsKey(aString);
+    }
+
+    public void showAll() {
+        for(String command : validCommands.keySet()) {
+            System.out.print(command + "  ");
+        }
+        System.out.println();
+    }
+}
