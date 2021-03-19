3068ad689b43319beb3491f976e8b386272cd9f6
Scott Love CSM2019 <scott.love.2019@uni.strath.ac.uk>
2021-03-19 09:38:50 +0000

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,6 +1,7 @@
 public class Player {
 	
 	private String username;
+	private double accuracy;
 	private int totalGuesses;
 	private int correctGuesses;
 	private int cryptogramsPlayed;
@@ -8,6 +9,7 @@ public class Player {
 	
 	public Player(String name) {
 		username = name;
+		accuracy = 0;
 		totalGuesses = 0;
 		cryptogramsPlayed = 0;
 		cryptogramsCompleted = 0;
@@ -30,27 +32,51 @@ public class Player {
 		correctGuesses++;
 	}
 	
-	public int getAccuracy() {
-		return ((correctGuesses / totalGuesses) * 100);
+	public double getAccuracy() {
+		return (double) ((correctGuesses / totalGuesses) * 100);
+	}
+	
+	public void setAccuracy(double set) {
+		accuracy = set;
 	}
 	
 	public int getNumCryptogramsCompleted() {
 		return cryptogramsCompleted;
 	}
 	
+	public void setNumCryptogramsCompleted(int set) {
+		cryptogramsCompleted = set;
+	}
+	
 	public int getNumCryptogramsPlayed() {
 		return cryptogramsPlayed;
 	}
 	
+	public void setNumCryptogramsPlayed(int set) {
+		cryptogramsPlayed = set;
+	}
+	
 	public int getGuesses() {
 		return totalGuesses;
+	}
+	
+	public void setGuesses(int set) {
+		totalGuesses = set;
 	}
 
 	public String getUsername() {
 		return username;
 	}
 	
+	public void setUsername(String name) {
+		username = name;
+	}
+	
 	public int getCorrectGuesses() {
 		return correctGuesses;
+	}
+	
+	public void setCorrectGuesses(int set) {
+		correctGuesses = set;
 	}
 }
