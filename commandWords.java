3c6ad223dd5b3ae7799f6c0aa529569350a2f146
Scott Love CSM2019 <scott.love.2019@uni.strath.ac.uk>
2021-03-27 21:16:49 +0000

    Added comments

--- commandWords.java
+++ commandWords.java
@@ -4,12 +4,12 @@ public class commandWords {
 	// A mapping between a command word and the commandWord associated with it 
 	private HashMap<String, commandWord> validCommands;
 	
+    // The constructor creates a new hashmap and then fills it with the strings which correspond to the commandWord
 	public commandWords() {
         validCommands = new HashMap<>();
         validCommands.put("guess", commandWord.GUESS);
         validCommands.put("save", commandWord.SAVE);
         validCommands.put("undo", commandWord.UNDO);
-        validCommands.put("hint", commandWord.HINT);
         validCommands.put("load", commandWord.LOAD);
         validCommands.put("exit", commandWord.EXIT);
         validCommands.put("help", commandWord.HELP);
@@ -20,9 +20,10 @@ public class commandWords {
         validCommands.put("leaderboard", commandWord.LEADERBOARD);
     }
 
+    // Returns either the command if it's valid and not-null or the UNKNOWN command
 	public commandWord getCommandWord(String commandWord) {
         commandWord command = validCommands.get(commandWord);
-        if(command != null) {
+        if (command != null) {
             return command;
         }
         else {
@@ -30,10 +31,12 @@ public class commandWords {
         }
     }
     
+    // Checks a string is a valid command
     public boolean isCommand(String aString) {
         return validCommands.containsKey(aString);
     }
 
+    // Prints all of the valid commands
     public void showAll() {
         for(String command : validCommands.keySet()) {
             System.out.print(command + " | ");
