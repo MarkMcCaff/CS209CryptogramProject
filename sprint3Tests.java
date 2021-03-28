097f92a5ed0ae197a1963d50fa0d225735b35719
Alexander Watson MCS2019 <a.watson.2019@uni.strath.ac.uk>
2021-03-28 12:30:12 +0000

    Leaderboard Test

--- sprint3Tests.java
+++ sprint3Tests.java
@@ -61,7 +61,61 @@ public class sprint3Tests {
             assertEquals(game.playerGuess[elements.get(i)],charSolution[elements.get(i)]);
         }
     }
+    @Test
+    void showLeaderboard() {
+    	File myObj = new File("leaderboardTest.txt");
+		
+		Double topScores[] = new Double [10];
+		String topPlayers[] = new String [10];
+	
+		Double max;
+		Double cryptogramsCompleted;
+		Double cryptogramsPlayed;
 
+		List<String> players = new ArrayList<String>();
+		List<Double> scores = new ArrayList<Double>();
+		List<String> words = new ArrayList<String>();
+		
+		try (Scanner sc = new Scanner((myObj), StandardCharsets.UTF_8.name())) {
+			while (sc.hasNextLine()) {
+				words.add(sc.nextLine());
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		for (String line : words) {
+			String[] split = line.split(",");
+			players.add(split[0]);
+			cryptogramsPlayed = (Double.parseDouble(split[4]));
+			cryptogramsCompleted = (Double.parseDouble(split[5]));
+		// Calculate proportion of successfully completed cryptograms
+			if(cryptogramsPlayed!=0) {
+				scores.add(cryptogramsCompleted / cryptogramsPlayed);
+			}else{
+				scores.add(0.0);
+			}
+		}
+		int size = 10;
+		// if there are less than 10 high scores, alter the amount
+		if(scores.size()<10){
+			size = scores.size();
+		}
+		max = 0.0;
+		// Find the highest score, add the player and score to our arrays and then remove them from the ArrayList
+		for(int i = 0; i < size; i++) {
+			max = Collections.max(scores);
+			topScores[i] = max;
+			int index = scores.indexOf(Collections.max(scores));
+			topPlayers[i] = players.get(index);
+			scores.remove(max);
+			players.remove(index);
+		}
+		for(int i = 0; i < 10; i++) {
+		assertEquals(topScores[0], Collections.max(scores));
+		assertEquals(topPlayers[i], players.get(topScores.get(i)));
+		}
+		
+    }
      @Test
     void emptyLeaderboard() {
     	File myObj = new File("NoPlayers.txt");
