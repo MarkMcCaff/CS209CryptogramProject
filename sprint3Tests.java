1c52b5975c53e38ce4264e26e4563b234ecf2fbb
Alexander Watson MCS2019 <a.watson.2019@uni.strath.ac.uk>
2021-03-28 12:08:04 +0000

    emptyLeaderboard Test

--- sprint3Tests.java
+++ sprint3Tests.java
@@ -1,8 +1,9 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-
+import java.util.ArrayList;
+import java.util.Scanner;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class sprint3Tests {
@@ -59,5 +60,54 @@ public class sprint3Tests {
         for(int i = 0;i<elements.size();i++){
             assertEquals(game.playerGuess[elements.get(i)],charSolution[elements.get(i)]);
         }
+    }
+
+     @Test
+    void emptyLeaderboard() {
+    	File myObj = new File("NoPlayers.txt");
+    	boolean required = false;
+    	Double topScores[] = new Double [10];
+		String topPlayers[] = new String [10];
+	
+		Double max;
+		Double cryptogramsCompleted;
+		Double cryptogramsPlayed;
+
+		List<String> players = new ArrayList<String>();
+		List<Double> scores = new ArrayList<Double>();
+		List<String> words = new ArrayList<String>();
+    	try (Scanner sc = new Scanner((myObj), StandardCharsets.UTF_8.name())) {
+			while (sc.hasNextLine()) {
+				words.add(sc.nextLine());
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+    	for (String line : words) {
+			String[] split = line.split(",");
+			players.add(split[0]);
+			cryptogramsPlayed = (Double.parseDouble(split[4]));
+			cryptogramsCompleted = (Double.parseDouble(split[5]));
+			if(cryptogramsPlayed!=0) {
+				scores.add(cryptogramsCompleted / cryptogramsPlayed);
+			}else{
+				scores.add(0.0);
+			}
+		}
+    	int size = 10;
+    	if(scores.size()<10){
+			size = scores.size();
+		}
+    	if (size==0){
+			System.out.println("Error - there are not any scores yet");
+			required = true;
+		}else {
+			for (int i = 0; i < size; i++) {
+				System.out.println((i + 1) + ": " + topPlayers[i] + " - " + topScores[i]);
+				required = false;
+			}
+		}
+
     }
 }
